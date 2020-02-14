8c364a6f9b23ee605dc9a8d33f1c406822f7e28b
rk11eder <rk11eder@gmail.com>
2020-02-14 09:53:13 +0000

    version 2

--- bot_flow.py
+++ bot_flow.py
@@ -2,121 +2,106 @@ import keyboard
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
-from datetime import date
 
 import time
-
-
+	
 def getNames(file_name):
-    nomes = []
-    file_name = "name_list.txt"
+	nomes=[]
+	file_name="name_list.txt"
 
-    # open file lorem.txt for reading text data
-    in_file = open(file_name, "rt")
+	in_file = open(file_name, "rt") # open file lorem.txt for reading text data
+	
 
-    for line in in_file:
-        nomes.append(line)
+	for line in in_file:
+		nomes.append(line)
 
-       # close the file
-    return nomes
-
-nomes = getNames("name_list.txt")
+               # close the file
+	return nomes 	
 
 
-today = str(date.today())
-usr = "andre"
-pas = "hubel123"
-municipio="Santiago do Cacem"
+usr = "leilamorenohairstylist"
+pas = "ederbarbosa"
 chrome_options = webdriver.ChromeOptions()
-prefs = {"profile.default_content_setting_values.notifications": 2}
-chrome_options.add_experimental_option("prefs", prefs)
-driver = webdriver.Chrome(executable_path="C:\\Users\eder.barbosa\Desktop\pyton\chromedriver.exe", chrome_options=chrome_options)
+prefs = {"profile.default_content_setting_values.notifications" : 2}
+chrome_options.add_experimental_option("prefs",prefs)
+driver= webdriver.Chrome(executable_path="C:\\Users\Eder Barbosa\Desktop\python\chromedriver.exe",chrome_options=chrome_options)
 
-
-driver.get("https://www.city-mind.com/default.aspx")
-
-elem = driver.find_element_by_name("AtusLogin$UserName")
+driver.get("https://www.instagram.com/accounts/login/")
+time.sleep(1)
+elem = driver.find_element_by_name("username")
 elem.send_keys(usr)
 time.sleep(1)
-elem = driver.find_element_by_name("AtusLogin$Password")
+elem = driver.find_element_by_name("password")
 elem.send_keys(pas)
 time.sleep(1)
 elem.send_keys(Keys.ENTER)
 time.sleep(2)
 
-elem = driver.find_element_by_name("ctl00$mainArea$cboMunicipals").clear()
-elem = driver.find_element_by_name("ctl00$mainArea$cboMunicipals");
-elem.send_keys(municipio)
+driver.get("https://www.instagram.com/mbeatrizquaresma/")
 
-time.sleep(1)
-elem = driver.find_element_by_name("ctl00$mainArea$btnOk");
+elem = driver.find_element_by_css_selector("._qv64e").text
 
-elem.click()
+print(elem);
 
-time.sleep(1)
-
+nomes=getNames("name_list.txt")
+tabVal=0;
+nameSub=[]
 for i in nomes:
-    print(nomes[nomes.index(i)]+"  "+i)
-
-    driver.get('https://www.city-mind.com/Pages/Meter/MeterCards.aspx?mid='+nomes[nomes.index(i)])
-    
-    
-    driver.find_element_by_id("ctl00_mainArea_meterdetails").click()
-  
-    
-  
-    nsMedidor = driver.find_element_by_name('ctl00$mainArea$meterDetailsUC1$IntbMeterSN').get_attribute('value')
- 
-    driver.find_element_by_name('ctl00$mainArea$meterDetailsUC1$IntbMeterSN').clear()
-    
-    
-    
-    nsMedidor = driver.find_element_by_name('ctl00$mainArea$meterDetailsUC1$IntbTransponderID').get_attribute('value')
-   
-    driver.find_element_by_name('ctl00$mainArea$meterDetailsUC1$IntbTransponderID').clear()
-    
-   
-    subzona = driver.find_element_by_name('ctl00$mainArea$meterDetailsUC1$cboSubZone').get_attribute('value')
-    
-
-    elem = driver.find_element_by_name('ctl00$mainArea$meterDetailsUC1$cboZone')
-    zona = elem.get_attribute('value')
-    elem.send_keys(Keys.ARROW_UP)
-
-  
-    varOb="ns "+nsMedidor +"\ntran "+  nsMedidor +"\nzona "+zona+"/sub z "+subzona+"\neliminiado",today
-    
-   
-    elem = driver.find_element_by_name("ctl00$mainArea$meterDetailsUC1$IntbRemarks").clear()
-    elem = driver.find_element_by_name('ctl00$mainArea$meterDetailsUC1$IntbRemarks').send_keys("ns "+nsMedidor +"\ntran "+  nsMedidor +"\nzona "+zona+"/sub z "+subzona+"\neliminiado "+today )
-
+	tabVal=0;
+	for x in range(40):
+			
+		tabVal+=1;
+		driver.get("https://www.instagram.com/")
+		time.sleep(0.2)
+		elem = driver.find_element_by_css_selector("._avvq0")
+		elem.send_keys(nomes[nomes.index(i)]) 
+		time.sleep(2)
+		print("dqwd1",tabVal)
+		
+		for x in range(tabVal):
+			keyboard.press_and_release('tab')
+			
+		
+		keyboard.press_and_release('enter')
+		
+		time.sleep(2)
+		try:
+			elem = driver.find_elements_by_class_name('_fd86t')[1]
+			num_seguidor=elem.get_attribute("title")
+			num_seguidor=num_seguidor.replace(" ", "")
+			num_seguindo = driver.find_elements_by_class_name('_fd86t')[2].text
+			num_seguindo=num_seguindo.replace(" ", "")
+			num_seguidor=float(num_seguidor)
+			num_seguindo=float(num_seguindo)
+			num_seguidor+=1
+			num_seguindo+=1
+			print(num_seguindo)
+			print(num_seguidor)
+			print(num_seguidor/num_seguindo)
+			elem = driver.find_element_by_css_selector("._qv64e").text
 
 
-    IntbCoordinateE = driver.find_element_by_name('ctl00$mainArea$meterDetailsUC1$IntbCoordinateE').get_attribute('value')
+			if elem=="Seguir":
+				
+				if num_seguidor/num_seguindo < 1.50:
+				
+					time.sleep(1)
+					elem = driver.find_element_by_css_selector("._qv64e")
+					elem.click()
+					elem = driver.find_element_by_css_selector("._rf3jb").text
+					print("nome:",elem)
+					a = open("nome_flow.txt","a")
+					print(elem, file=a)  
+					a.close
+					time.sleep(0.1)	
+			
+				
 
-    IntbCoordinateN = driver.find_element_by_name('ctl00$mainArea$meterDetailsUC1$IntbCoordinateN').get_attribute('value')
-
-    driver.find_element_by_name('ctl00$mainArea$meterDetailsUC1$IntbCoordinateE').clear()
-    driver.find_element_by_name('ctl00$mainArea$meterDetailsUC1$IntbCoordinateN').clear()
-
-    IntbCoordinateE= IntbCoordinateE.replace(",", ".")
-
-    IntbCoordinateN= IntbCoordinateN.replace(",", ".")
-
-    if IntbCoordinateE != "":
-        
-        driver.find_element_by_name('ctl00$mainArea$meterDetailsUC1$IntbCoordinateE').send_keys(IntbCoordinateE)
-        driver.find_element_by_name('ctl00$mainArea$meterDetailsUC1$IntbCoordinateN').send_keys(IntbCoordinateN)
-    
-   
+		except IndexError:
+			print("Errro garago")
+		
 
 
 
- 
-   
-    #elem = driver.find_element_by_id("ShowUpdateDiv")
-   # elem = driver.find_element_by_name("ctl00$mainArea$btnUpdate")
- 
-    #elem.click();    
-    time.sleep(10)
+time.sleep(2)
 driver.quit()
